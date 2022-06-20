bee15105931f10ac3e78b3a16daaf497b2b5b592
Alex Linden <alextl2004@gmail.com>
2022-06-20 12:32:23 -0700

    part one and two before lunch

--- app.py
+++ app.py
@@ -1,1 +1,64 @@
 """Flask app for Cupcakes"""
+from flask import Flask, jsonify, request
+#url_for, render_template, redirect, flash
+
+#from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+connect_db(app)
+db.create_all()
+
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+#toolbar = DebugToolbarExtension(app)
+
+@app.get('/api/cupcakes')
+def list_all_cupckakes():
+    """return JSON {'cupakes': [{id, flavor, size, rating, image_url}, ...]}"""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+@app.get('/api/cupcakes/<int:cupcake_id>')
+def list_single_cupcake(cupcake_id):
+    """return JSON {'cupake': [{id, flavor, size, rating, image_url}"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+@app.post('/api/cupcakes')
+def create_new_cupcake():
+    """return JSON for new cupcake {'cupake': [{id, flavor, size, rating, image_url}"""
+
+    flavor = request.json['flavor']
+    size = request.json['size']
+    rating = request.json['rating']
+    image_url = request.json['image_url']
+
+    new_cupcake = Cupcake(
+                        flavor=flavor,
+                        size=size,
+                        rating=rating,
+                        image_url=image_url
+                        )
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
--- models.py
+++ models.py
@@ -1,1 +1,45 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+DEFAULT_IMAGE = "https://tinyurl.com/demo-cupcake"
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Cupcake"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default='')
+
+    def image_url(self):
+        """Return image for cupcake -- bespoke or default."""
+
+        return self.image_url or DEFAULT_IMAGE
+
+    def serialize(self):
+        """Serialize to dictionary."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image_url": self.image_url
+        }
+
+
